a1e24769d797651d582cb3f70d91807a70216c75
Roberto <roberto.previtera@gmail.com>
2022-06-06 17:31:45 +0200

    Update upload.sh
    avrdude 7.0

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,3 +1,3 @@
 echo "uploading firmware to atmega"
-sudo avrdude -P /dev/spidev0.0 -c linuxspi -p m328p -U lfuse:w:0xE2:m -U hfuse:w:0xD9:m -U efuse:w:0xFF:m -U lock:w:0xFF:m -U flash:w:firmware/atmega.hex
+sudo avrdude -P /dev/spidev0.0;/dev/gpiochip0 -c linuxspi -p m328p -U lfuse:w:0xE2:m -U hfuse:w:0xD9:m -U efuse:w:0xFF:m -U lock:w:0xFF:m -U flash:w:firmware/atmega.hex
 touch firmware/initialised
